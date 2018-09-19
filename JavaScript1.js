37141e0d2fc3c501abdf924ca93c583af0e81ac5
venukalam <30835776+venukalam@users.noreply.github.com>
2018-09-19 09:51:39 +0530

    Add files via upload

---
+++ JavaScript1.js
@@ -0,0 +1,15 @@
+﻿
+$(document).ready(function () {
+    var text_value1 = $("#text1").val();
+
+    var text_value2 = $("#text2").val();
+
+    var result = text_value1 + text_value2;
+
+    $("button").click(function () {
+
+        alert('result is'+result);
+    });
+
+});
+
